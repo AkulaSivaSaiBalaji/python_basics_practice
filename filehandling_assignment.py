2ab1eb78e4a4865193801c46dface00f77ca898f
AkulaSivaSaiBalaji <akulasivasaibalaji47@gmail.com>
2026-09-20 11:58:22 +0530

    filehandling- assignment completed

--- filehandling_assignment.py
+++ filehandling_assignment.py
@@ -65,3 +65,75 @@ with open('attendance','r') as file:
             print(i.replace('P',''))
 '''
 
+'''
+with open('inventory.txt','a') as file:
+    try:
+        for i in range(3):
+            product_name=input("Enter product name: ")
+            quantity=input("Enter Quantity: ")
+            copy_qnty=int(quantity)
+            if copy_qnty>=0:
+                file.write(';'+product_name+',')
+                file.write(quantity)
+            else:
+                print("enter quantity grater than 1 !!")
+    except ValueError:
+        print("invalid values entered")
+
+try:
+    with open('inventory.txt','r') as file:
+        inv=file.read().split(';')
+        print()
+        print("Current Inventory: ")
+        search=[]
+        for items in inv:
+            print(items.replace(',',' - '))
+            search.append(items)
+            search.append(items.lower())
+            search.append(items.upper())
+        print()
+        search_item=input("Enter Item to search in inventory: ")
+        lower_search_item=search_item.lower()
+        upper_search_item=search_item.upper()
+        for i in search:
+            if search_item in i or lower_search_item in i or upper_search_item in i:
+                print(search_item+" is available.")
+                print(i.replace(',',': '))
+                break
+except FileNotFoundError:
+    print("unable to find the file")
+'''
+
+try:
+    with open('students.txt','r') as file:
+        passed_count=0
+        failed_count=0
+        total_passed_marks=0
+        total_failed_marks=0
+        for record in file.readlines():      
+            details=record.split(",")
+            student_name=details[0]
+            try:
+                marks=int(details[1].strip())
+                if marks>=50:
+                    print(student_name," - ",marks,' - Pass') 
+                    passed_count+=1
+                    total_passed_marks=total_passed_marks+marks
+                else:
+                    failed_count+=1
+                    total_failed_marks=total_failed_marks+marks
+                    print(student_name," - ",marks,' - Fail')
+            except ValueError:
+                 print(f"invalid marks for {student_name}")
+                 print()
+        sum=total_passed_marks+total_failed_marks
+        count=passed_count+failed_count
+        avg_marks=sum/count
+        print("-------------------------") 
+        print("Result Summary")
+        print("-------------------------")
+        print(f"Passed Students :{passed_count}")
+        print(f"Failed Students :{failed_count}")
+        print(f"Avarage Mark :{avg_marks}")
+except FileNotFoundError:
+    print("file not found")
